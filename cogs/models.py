a3ac2eadcb1d8496abbec0292a6d0dc4b25da4d9
nit <148101653+nitl0l@users.noreply.github.com>
2023-11-04 17:08:39 -0500

    fix: Remove unnecessary average_rating property from Employee

--- cogs/models.py
+++ cogs/models.py
@@ -1,18 +1,13 @@
-from tortoise.models import Model
 from tortoise import fields
+from tortoise.models import Model
 
 
 class Employee(Model):
     id = fields.UUIDField(pk=True)
     name = fields.CharField(max_length=30, unique=True, index=True)
-    average_rating = fields.FloatField(default=0.0)
 
 
 class Review(Model):
     employee_id = fields.UUIDField()
     rating = fields.FloatField()
     description = fields.TextField()
-
-
-def setup(_):
-    pass
